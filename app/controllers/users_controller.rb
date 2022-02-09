65c4d5ef17363b1ac6f39bcf5c72a43e9573ba72
Paul Tobin <tobster111@gmail.com>
2022-02-09 18:27:15 +0000

    Edit and update users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,16 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    
+  end
+  
+  
   
   def create
     @user = User.new(user_params)
