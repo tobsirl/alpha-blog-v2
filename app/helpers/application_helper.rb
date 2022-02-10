67b6ba72dd24790d2e0b0d46d9542ee39cc435be
Paul Tobin <tobster111@gmail.com>
2022-02-10 19:42:58 +0000

    profile image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,13 @@
 module ApplicationHelper
-  def gravatar_for(user)
+  def gravatar_for(user, options = { size: 80 })
     email_address = user.email.downcase
 
     hash = Digest::MD5.hexdigest(email_address)
 
-    gravatar_url = "https://www.gravatar.com/avatar/#{hash}"
-    
-    image_tag(gravatar_url, alt: user.username)
+    size = options[:size]
+
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+
+    image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
   end
 end
