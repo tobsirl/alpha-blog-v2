fb6873fb0288f08989892861475eb5c2a662ad1b
Paul Tobin <tobster111@gmail.com>
2022-02-13 10:14:09 +0000

    Create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      flash[:notice] = "Logged in successfully"
+      redirect_to user
+    else 
+      flash.now[:alert] = "Incorrect log in details."
+      render 'new'
+    end
   end
 
   def destroy
