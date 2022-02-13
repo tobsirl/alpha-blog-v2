742038f77e4ad32300a39b663569910edb851eef
Paul Tobin <tobster111@gmail.com>
2022-02-13 10:26:08 +0000

    destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,6 +16,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    
+    session[:user_id] = nil
+    flash[:notice] = "Logged out"
+    redirect_to root_path
   end
 end
