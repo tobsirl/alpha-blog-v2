bda39ba92864c5d74e12d9abcd1e336434e02e7e
Paul Tobin <tobster111@gmail.com>
2022-02-13 12:16:47 +0000

    Added the private method set_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user
+
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -13,11 +14,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated"
       redirect_to @user
@@ -42,6 +41,7 @@ class UsersController < ApplicationController
     params.require(:user).permit(:username, :email, :password)
   end
   
-  
-
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
