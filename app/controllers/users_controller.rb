d2869b180890fa8a5caf1d41aa930093af579af6
Paul Tobin <tobster111@gmail.com>
2022-02-17 19:44:26 +0000

    destroy method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :update, :show]
+  before_action :set_user, only: [:edit, :update, :show, :destroy]
   before_action :require_user, only: [:edit, :update]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
@@ -37,6 +37,14 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def destroy
+    @user.destory
+    session[:user_id] = nil
+    flash[:notice] = "Account and all associated articles successfully deleted"
+    redirect_to articles_path
+  end
+  
 
   private
   def user_params
